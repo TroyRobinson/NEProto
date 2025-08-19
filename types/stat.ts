852f51eeb0b14e57f0c3981f35393c446a27913e
Troy Robinson <troy@lionsrock.com>
2025-08-19 13:50:12 -0500

    Guard stat search against missing fields

---
+++ types/stat.ts
@@ -0,0 +1,11 @@
+export interface Stat {
+  id: string;
+  variableId: string;
+  title: string;
+  description: string;
+  category: string;
+  dataset: string;
+  source: string;
+  year: number;
+  data: string;
+}
